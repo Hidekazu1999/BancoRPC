f1191196dafe606d1b30f7ae65ce62cf7f109a11
Hidekazu Takayama <hidekazutakayama_lindo@hotmail.com>
2022-01-20 18:37:51 -0300

    Atividade RPC com RMI

--- src/com/gugawag/rpc/banco/AppClienteBanco.java
+++ src/com/gugawag/rpc/banco/AppClienteBanco.java
@@ -1,7 +1,6 @@
 package com.gugawag.rpc.banco;
 
 import java.net.MalformedURLException;
-import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -11,7 +10,8 @@ import java.util.Scanner;
 public class AppClienteBanco {
 
     public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
-        // procura o serviço no RMI Registry local. Perceba que o cliente não connhece a implementação do servidor,
+        // procura o serviço no RMI Registry local. Perceba que o cliente não connhece a
+        // implementação do servidor,
         // apenas a interface
         Registry registry = LocateRegistry.getRegistry();
         BancoServiceIF banco = (BancoServiceIF) registry.lookup("BancoService");
@@ -20,17 +20,28 @@ public class AppClienteBanco {
         Scanner entrada = new Scanner(System.in);
         int opcao = entrada.nextInt();
 
-        while(opcao != 9) {
+        while (opcao != 9) {
             switch (opcao) {
+
+                case 0: {
+                    banco.adicionarConta();
+                    System.out.println("Conta criada com sucesso");
+                }
                 case 1: {
                     System.out.println("Digite o número da conta:");
                     String conta = entrada.next();
-                    //chamada ao método remoto, como se fosse executar localmente
+                    // chamada ao método remoto, como se fosse executar localmente
                     System.out.println(banco.saldo(conta));
                 }
                 case 2: {
-                    //chamada ao método remoto, como se fosse executar localmente
+                    // chamada ao método remoto, como se fosse executar localmente
                     System.out.println(banco.quantidadeContas());
+                }
+                case 3: {
+                    System.out.println("Digite o número da conta:");
+                    String conta = entrada.next();
+                    banco.removerConta(conta);
+                    System.out.println("Conta removida com sucesso");
                 }
             }
             menu();
@@ -40,8 +51,10 @@ public class AppClienteBanco {
 
     public static void menu() {
         System.out.println("\n=== BANCO RMI (ou FMI?!) ===");
+        System.out.println("0 - Criar conta");
         System.out.println("1 - Saldo da conta");
         System.out.println("2 - Quantidade de contas");
+        System.out.println("3 - Remover conta");
         System.out.println("9 - Sair");
     }
 
--- src/com/gugawag/rpc/banco/BancoServiceServer.java
+++ src/com/gugawag/rpc/banco/BancoServiceServer.java
@@ -2,12 +2,20 @@ package com.gugawag.rpc.banco;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
+
+import com.gugawag.rpc.banco.model.Conta;
 
 public class BancoServiceServer extends UnicastRemoteObject implements BancoServiceIF {
 
     private Map<String, Double> saldoContas;
+
+    private List<Conta> contas = new ArrayList<Conta>();
+    Random random = new Random();
 
     public BancoServiceServer() throws RemoteException {
         saldoContas = new HashMap<>();
@@ -24,6 +32,21 @@ public class BancoServiceServer extends UnicastRemoteObject implements BancoServ
     @Override
     public int quantidadeContas() throws RemoteException {
         return saldoContas.size();
+
+    }
+
+    @Override
+    public void adicionarConta() throws RemoteException {
+        int numero = random.nextInt(9);
+        Conta conta = new Conta(String.valueOf(numero), 0.0);
+        contas.add(conta);
+
+    }
+
+    @Override
+    public void removerConta(String numero) throws RemoteException {
+        saldoContas.remove(numero);
+
     }
 
 }
